278402df245443ce4147117578a036737a7cae39
paccionesawyer <57788768+paccionesawyer@users.noreply.github.com>
2021-06-23 09:54:06 -0400

    Added Initialization of Serial Device on Port D

--- interfaces/Spike-Prime/initialize.py
+++ interfaces/Spike-Prime/initialize.py
@@ -10,6 +10,14 @@ colorSensor = forceSensor = distanceSensor = -1
 lengths = [0,0,0,0,0,0]
 portType = ["none", "none", "none", "none", "none", "none"]
 
+serial_dev = hub.port.D
+serial_dev.mode(hub.port.MODE_FULL_DUPLEX)
+
+utime.sleep(1)
+serial_dev.baud(115200)
+serial_dev.write("Initialized...\r\n")
+hub.display.show(hub.Image.YES)
+
 # Each type of motor/sensor has a different array size, so we can figure out
 # which port has what on it based on how long the array is
 for i in range(6):
